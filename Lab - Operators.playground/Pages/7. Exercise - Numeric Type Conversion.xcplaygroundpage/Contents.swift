40d66d32e9180a7f651adc270932a1088d1479d5
jonas <filerjo@quincy.edu>
2021-07-15 10:01:41 -0500

    Initial commit

---
+++ Lab - Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+let x: Int = 10
+let y: Double = 3.2
+let multipliedAsIntegers = <#value#>
+
+
+/*:
+ Create a constant `multipliedAsDoubles` equal to `x` times `y`, but this time convert the `Int` to a `Double` in the expression. Print the result.
+ */
+let multipliedAsDoubles = 
+
+
+/*:
+ Are the values of `multipliedAsIntegers` and `multipliedAsDoubles` different? Print a statement to the console explaining why.
+ */
+
+
+//: [Previous](@previous)  |  page 7 of 8  |  [Next: App Exercise - Converting Types](@next)
